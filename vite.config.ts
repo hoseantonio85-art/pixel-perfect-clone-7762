976caf1a5d8576322e27b9f7e910595598370f38
hoseantonio85-art <hoseantonio85@gmail.com>
2026-03-17 14:11:49 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig(({ mode }) => ({
     },
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  base: 'pixel-perfect-clone-7762',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
